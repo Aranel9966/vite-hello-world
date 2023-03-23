11626c2004b210d2f4b973e623383419c5d4be9f
Aranel9966 <99angelo.dire@gmail.com>
2023-03-23 14:17:13 +0100

    add component

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script >
+//esporto tutto il componente
+export default{
+    data(){
+        return{
+            title:'ciao'
+        }
+    }
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+
+h1{
+    color: red;
+}
+
+</style>
