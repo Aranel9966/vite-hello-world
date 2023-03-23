8ba38c5a68c5323b3ced29a4eccb05dc6f30adf7
Aranel9966 <99angelo.dire@gmail.com>
2023-03-23 14:38:17 +0100

    add bonus with 2 methods

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@
 
 <template>
   <AppTitle/>
+  <AppHero/>
 </template>
 
 <style scoped>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,25 @@
+<script>
+export default{
+    data(){
+        return{
+            imgName:'04.webp'
+        }
+    },
+    methods: {
+        //creo una funzione che che indipendentemente dall'immagine sa sempre il percorso
+      getImgePath:function(imgPath){
+        return new URL(`../assets/img/${imgPath}`, import.meta.url).href;
+      }  
+    },
+}
+</script>
+
+<template>
+    <div>
+        <img src="../assets/img/04.webp" >
+        <img :src="getImgePath(imgName)" >
+    </div>
+</template>
+
+<style>
+</style>
